615f59f7682d9e4f28d02d16810ee0ce65c1fd0b
Daniel Montoya <dacamoty@gmail.com>
2026-03-15 22:33:20 -0500

    Correction workers

--- worker/src/services/payment-web-constants.ts
+++ worker/src/services/payment-web-constants.ts
@@ -1,4 +1,4 @@
-export const PAYMENT_SHIPMENT_URL = "https://www3.interrapidisimo.com/WebExterno/Consulta/Carga.aspx";
+export const PAYMENT_SHIPMENT_URL = "https://www3.interrapidisimo.com/SiguetuEnvio/shipment";
 export const PAYMENT_API_RESPONSE_URL_PART = "ObtenerRastreoGuiasClientePost";
 export const PAYMENT_GUIDE_INPUT_SELECTOR = "#inputGuide";
 export const PAYMENT_PRIMARY_SEARCH_BUTTON_SELECTOR = "#btnRastrear";
